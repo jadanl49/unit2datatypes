7af0fa81815139d6f2c76d9e0dc5f6aa3330393c
jadanl49 <jadanl3@nycstudents.net>
2026-09-25 09:24:07 -0400

    Update tip calc.py

--- tip calc.py
+++ tip calc.py
@@ -1,17 +1,44 @@
+f=int(input("What is your number"))
+result = f%2
+if result == 1:
+    print("odd")
+else:
+    print("even")
+
 """ def bilcalc(b):
     b=str(input("how much was the bill"))
     if b == "bad":
         print("0%")
     elif b == "okay":
         print("15%")
-    elif b =="good":
+    elif b =="good":             #bill
         print("20%")
     elif b == "great":
         print("25%")
 bilcalc(b=True) """
 
-f=float(input("What is your number?"))
+""" f=int(input("What is your number?"))
 result = f/1
-result = f/2
+a = f//2
+b = f//3
+c = f//4              #factors
+d = f//5
+e = f//6
+g = f//7
+h = f//8
+i = f//9
+print(result,a,b,c,d,e,g,h,i) """
 
-print(result)
+""" f, z = map(int, input("What is your first factor, what is the second?").split())
+a = f/1
+b = f/2
+c = f/3
+d = f/4              #factors
+e = f/5
+g = f/6
+h = f/7
+i = f/8
+j = f/9
+print(f"First number: {f}, Second number: {z}")   #print using f string """ 
+
+
